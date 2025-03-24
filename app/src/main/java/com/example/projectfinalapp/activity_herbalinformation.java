3eac81fc30270f5b49a57b1a4f4273774be8a947
GuGusan2 <samethestar01@gmail.com>
2025-03-24 15:32:20 +0700

    Spinner Update

--- app/src/main/java/com/example/projectfinalapp/activity_herbalinformation.java
+++ app/src/main/java/com/example/projectfinalapp/activity_herbalinformation.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -17,7 +19,9 @@ import androidx.core.view.WindowInsetsCompat;
 public class activity_herbalinformation extends AppCompatActivity {
     Spinner myselect;
     TextView myfullname;
-    ImageView myandor,myindian,myturmeric,myginger,myhome,mylove,mypro;
+
+    Button myseemore;
+    ImageView myandor,myindian,myturmeric,myginger,myhome,mylove,mypro,myshow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,7 @@ public class activity_herbalinformation extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_herbalinformation);
 
+        myseemore = findViewById(R.id.seemoreBT);
         myselect = findViewById(R.id.selectherb);
         myfullname = findViewById(R.id.emailTV);
         myandor = findViewById(R.id.androIV);
@@ -34,9 +39,142 @@ public class activity_herbalinformation extends AppCompatActivity {
         myhome = findViewById(R.id.homeInfoIV);
         mylove = findViewById(R.id.loveInfoIV);
         mypro = findViewById(R.id.proInfoIV);
+        myshow = findViewById(R.id.imageshow);
 
         //Step 3 spinner select herb
+        myselect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String sp1 = myselect.getSelectedItem().toString();
+                switch (sp1) {
+                    case "ฟ้าทะลายโจร": {
+                        myshow.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.andrographispaniculata));
+                        myseemore.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(getApplicationContext(),AndrographiPaniculata.class);
+                                startActivity(intent);
+                            }
+                        });
 
+                        break;
+                    }
+                    case "กระชาย": {
+                        myshow.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.boesenbergiarotunda));
+                        myseemore.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(getApplicationContext(),Boesenbergia_rotunda.class);
+                                startActivity(intent);
+                            }
+                        });
+
+                        break;
+                    }
+                    case "ขิง": {
+                        myshow.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ginger1));
+                        myseemore.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(getApplicationContext(),Ginger.class);
+                                startActivity(intent);
+                            }
+                        });
+
+                        break;
+                    }
+                    case "กะเพราแดง": {
+                        myshow.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.holybasil));
+                        myseemore.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(getApplicationContext(),Holy_Basil.class);
+                                startActivity(intent);
+                            }
+                        });
+
+                        break;
+                    }
+                    case "มะขามป้อม": {
+                        myshow.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.indiangooseberry));
+                        myseemore.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(getApplicationContext(),Indian_Gooseberry.class);
+                                startActivity(intent);
+                            }
+                        });
+
+                        break;
+                    }
+                    case "ตะไคร้": {
+                        myshow.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.lemongrass1));
+                        myseemore.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(getApplicationContext(),Lemongrass.class);
+                                startActivity(intent);
+                            }
+                        });
+
+                        break;
+                    }
+                    case "ชะเอมเทศ": {
+                        myshow.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.licoriceroot));
+                        myseemore.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(getApplicationContext(),Licorice_Root.class);
+                                startActivity(intent);
+                            }
+                        });
+
+                        break;
+                    }
+                    case "หญ้าหนวดแมว": {
+                        myshow.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.orthosiphonaristatus));
+                        myseemore.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(getApplicationContext(),Orthosiphon_aristatus.class);
+                                startActivity(intent);
+                            }
+                        });
+
+                        break;
+                    }
+                    case "ใบเตย": {
+                        myshow.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.pandanleaf));
+                        myseemore.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(getApplicationContext(),Pandan_Leaf.class);
+                                startActivity(intent);
+                            }
+                        });
+
+                        break;
+                    }
+                    case "ขมิ้นชัน": {
+                        myshow.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.turmeric1));
+                        myseemore.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(getApplicationContext(),Turmeric.class);
+                                startActivity(intent);
+                            }
+                        });
+
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         //Step 3 myhome imageview go to andro page
         myhome.setOnClickListener(new View.OnClickListener() {
